b18ff11cef795115ad2d00bef1fa56278c82e327
Cyrus Kiprotich <cyruskiprotich8@gmail.com>
2025-03-27 10:31:41 +0300

    hooks cron error fix

--- event_streaming/hooks.py
+++ event_streaming/hooks.py
@@ -130,9 +130,11 @@ scheduler_events = {
 	# "monthly": [
 	# 	"event_streaming.tasks.monthly"
 	# ],
-  "1/2 * * * *":[
-        "event_streaming.terminology.drug_templates.import_item_from_uat"
-    ],
+	"cron": {
+		"1/2 * * * *":[
+			"event_streaming.terminology.drug_templates.import_item_from_uat"
+		],
+	}
 }
 
 # Testing
